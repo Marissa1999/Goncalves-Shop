843518ac0ddf0fe396fda2798a0ddc0fceb044d8
Marissa1999 <marissagoncalves13@gmail.com>
2019-09-10 13:54:06 -0400

    Started the Menu Activity.

--- app/src/main/java/com/example/goncalvesshop/MainActivity.java
+++ app/src/main/java/com/example/goncalvesshop/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.goncalvesshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    public static final int TEXT_REQUEST = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,4 +18,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void launchSecondActivity(View view)
+    {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+
 }
