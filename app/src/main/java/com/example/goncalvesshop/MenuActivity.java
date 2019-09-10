e716d74f7b2a859c7d41dd84c5ada8969d0e3a91
Marissa1999 <marissagoncalves13@gmail.com>
2019-09-10 14:07:49 -0400

    Added more CardViews.

--- app/src/main/java/com/example/goncalvesshop/MenuActivity.java
+++ app/src/main/java/com/example/goncalvesshop/MenuActivity.java
@@ -1,13 +1,23 @@
 package com.example.goncalvesshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+
+    public static final int TEXT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void launchSecondActivity(View view)
+    {
+        Intent intent = new Intent(this, CheckoutActivity.class);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 }
