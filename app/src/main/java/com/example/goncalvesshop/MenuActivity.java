5759ce3050e772da91eecfc3076cdb86ddc4067d
Marissa1999 <marissagoncalves13@gmail.com>
2019-09-13 15:45:12 -0400

    Added new methods to add and subtract quantity, and added a ConstraintLayout for the first CardView.

--- app/src/main/java/com/example/goncalvesshop/MenuActivity.java
+++ app/src/main/java/com/example/goncalvesshop/MenuActivity.java
@@ -4,22 +4,49 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity
 {
 
     public static final int TEXT_REQUEST = 1;
+    private int addedQuantity = 0;
+    private int subtractedQuantity = 0;
+    private TextView showAddedQuantity;
+    private TextView showSubtractedQuantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        showAddedQuantity = (TextView) findViewById(R.id.album_quantity_1);
+        showSubtractedQuantity = (TextView) findViewById(R.id.album_quantity_1);
     }
+
 
     public void launchCheckoutActivity(View view)
     {
         Intent checkoutIntent = new Intent(this, CheckoutActivity.class);
         startActivityForResult(checkoutIntent, TEXT_REQUEST);
     }
+
+
+    public void addQuantity(View view)
+    {
+        addedQuantity++;
+        if (showAddedQuantity != null)
+            showAddedQuantity.setText(Integer.toString(addedQuantity));
+
+    }
+
+
+    public void subtractQuantity(View view)
+    {
+        subtractedQuantity--;
+        if (showSubtractedQuantity != null)
+            showSubtractedQuantity.setText(Integer.toString(subtractedQuantity));
+
+    }
+
 }
