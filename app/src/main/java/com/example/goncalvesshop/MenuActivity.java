60bbdce5ec7f7f283e66f7670dde1a26c09b43e6
Marissa1999 <marissagoncalves13@gmail.com>
2019-09-13 18:28:48 -0400

    Edited the MenuActivity file again.

--- app/src/main/java/com/example/goncalvesshop/MenuActivity.java
+++ app/src/main/java/com/example/goncalvesshop/MenuActivity.java
@@ -10,18 +10,18 @@ public class MenuActivity extends AppCompatActivity {
 
     public static final int TEXT_REQUEST = 1;
     private int quantity = 0;
-    private double subtotal = 0.00;
+    private double subtotal = 0;
 
 
-    //private TextView idPrice =(TextView) findViewById(R.id.album_price_1).getText().toString();
-    //private String printedPrice = idPrice.getText().toString();
-    ///private double price = Double.parseDouble(printedPrice);
+    private TextView idPrice =(TextView) findViewById(R.id.album_price_1);
+    private String printedPrice = idPrice.getText().toString();
+    private double price = Double.parseDouble(printedPrice);
 
 
     private TextView showAddedQuantity;
     private TextView showSubtractedQuantity;
-    //private TextView showAddedSubtotal;
-    //private TextView showSubtractedSubtotal;
+    private TextView showAddedSubtotal;
+    private TextView showSubtractedSubtotal;
 
 
     @Override
@@ -30,8 +30,8 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         this.showAddedQuantity = (TextView) findViewById(R.id.album_quantity_1);
         this.showSubtractedQuantity = (TextView) findViewById(R.id.album_quantity_1);
-        //this.showAddedSubtotal = (TextView) findViewById(R.id.album_subtotal_1);
-        //this.showSubtractedSubtotal = (TextView) findViewById(R.id.album_subtotal_1);
+        this.showAddedSubtotal = (TextView) findViewById(R.id.album_subtotal_1);
+        this.showSubtractedSubtotal = (TextView) findViewById(R.id.album_subtotal_1);
     }
 
 
@@ -73,15 +73,15 @@ public class MenuActivity extends AppCompatActivity {
     }
 
 
-/*
+
     public void addSubtotal(View view)
     {
 
         this.subtotal += this.quantity * this.price;
 
-        if (this.subtotal < 0.00)
+        if (this.subtotal < 0)
         {
-            this.subtotal = 0.00;
+            this.subtotal = 0;
         }
 
         if (this.showAddedSubtotal != null)
@@ -95,21 +95,15 @@ public class MenuActivity extends AppCompatActivity {
 
         this.subtotal -= this.quantity * this.price;
 
-        if (this.subtotal < 0.00)
+        if (this.subtotal < 0)
         {
-            this.subtotal = 0.00;
+            this.subtotal = 0;
         }
 
         if (this.showSubtractedSubtotal != null)
             this.showSubtractedSubtotal.setText(Double.toString(this.subtotal));
 
     }
-
-*/
-
-
-
-
 
 
 }
