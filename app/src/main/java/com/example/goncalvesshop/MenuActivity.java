30ec3343b217a3b300a46a0339b77a32cbc42b9f
Marissa1999 <marissagoncalves13@gmail.com>
2019-09-16 12:03:10 -0400

    Added more features to the CheckoutActivity.

--- app/src/main/java/com/example/goncalvesshop/MenuActivity.java
+++ app/src/main/java/com/example/goncalvesshop/MenuActivity.java
@@ -1,13 +1,11 @@
 package com.example.goncalvesshop;
 
 import android.content.Intent;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MenuActivity extends AppCompatActivity {
@@ -15,37 +13,37 @@ public class MenuActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MenuActivity.class.getSimpleName();
     public static final int TEXT_REQUEST = 1;
-    public static final String EXTRA_CART = "com.example.goncalvesshop.extra.CART";
-    private ShoppingCart cart;
+    private double finalAlbumSubtotal;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        cart = new ShoppingCart();
     }
 
 
     public void modifyAlbumQuantity(View view) {
 
+
         ViewGroup item = (ViewGroup) view.getParent();
         TextView albumQuantity = item.findViewById(R.id.album_quantity);
         TextView albumPrice = item.findViewById(R.id.album_price);
         TextView albumSubtotal = item.findViewById(R.id.album_subtotal);
-        TextView albumTitle = item.findViewById(R.id.album_title);
-        TextView albumDescription = item.findViewById(R.id.album_description);
         TextView addingButton = item.findViewById(R.id.adding_button);
         TextView minusButton = item.findViewById(R.id.minus_button);
 
+
         String stringAlbumQuantity = albumQuantity.getText().toString();
         int integerAlbumQuantity = Integer.parseInt(stringAlbumQuantity);
+
 
         if (view.getId() == addingButton.getId())
         {
             integerAlbumQuantity++;
             showAlbumQuantity(integerAlbumQuantity, albumQuantity);
             showAlbumSubtotal(integerAlbumQuantity, albumPrice, albumSubtotal);
+            addFinalSubtotal(albumSubtotal);
         }
 
         else if (view.getId() == minusButton.getId())
@@ -53,14 +51,6 @@ public class MenuActivity extends AppCompatActivity {
             integerAlbumQuantity--;
             showAlbumQuantity(integerAlbumQuantity, albumQuantity);
             showAlbumSubtotal(integerAlbumQuantity, albumPrice, albumSubtotal);
-
-            String initialStringAlbumPrice = albumPrice.getText().toString();
-            String stringAlbumPrice = initialStringAlbumPrice.substring(1);
-            double integerAlbumPrice = Double.parseDouble(stringAlbumPrice);
-
-            cart.addAlbum(albumTitle.getText().toString(), albumDescription.getText().toString(), integerAlbumPrice, integerAlbumQuantity);
-
-            Toast.makeText(this, cart.getAlbumString(0), Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -74,6 +64,7 @@ public class MenuActivity extends AppCompatActivity {
         albumSubtotal.setText(String.format("$%s", newAlbumSubtotal));
     }
 
+
     private void showAlbumQuantity(int integerAlbumQuantity, TextView albumQuantity)
     {
         String convertedAlbumQuantity = Integer.toString(integerAlbumQuantity);
@@ -81,10 +72,20 @@ public class MenuActivity extends AppCompatActivity {
     }
 
 
+    private void addFinalSubtotal(TextView albumSubtotal)
+    {
+        String initialStringAlbumSubtotal = albumSubtotal.getText().toString();
+        String stringAlbumSubtotal = initialStringAlbumSubtotal.substring(1);
+        double newAlbumSubtotal = Double.parseDouble(stringAlbumSubtotal);
+        this.finalAlbumSubtotal += newAlbumSubtotal;
+        albumSubtotal.setText(String.format("$%s", newAlbumSubtotal));
+    }
+
+
     public void launchCheckoutActivity(View view)
     {
         Intent checkoutIntent = new Intent(this, CheckoutActivity.class);
-        checkoutIntent.putExtra(EXTRA_CART, cart);
+        checkoutIntent.putExtra("Subtotal", this.finalAlbumSubtotal);
         startActivityForResult(checkoutIntent, TEXT_REQUEST);
     }
 
